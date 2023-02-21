efc6656f210eeeb570111eb5d4b353ed4830a7f0
burningashes666 <126013091+burningashes666@users.noreply.github.com>
2023-02-21 10:28:16 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,26 @@
+let optionOnebutton = ("Leon"); 
+let optionTwobutton = ("Ashley");
+
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+<button type="button" onclick=
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
